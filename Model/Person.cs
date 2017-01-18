92b54a47db2abb44d87fae574125a664de7cb54f
Milan Vasic <vaskee@elfak.rs>
2017-01-18 14:04:26 +0100

    Minor edits

--- Model/Person.cs
+++ Model/Person.cs
@@ -11,7 +11,7 @@ namespace NeoProject.Model
         String name     { get; set; }
         String lastname { get; set; }
         String sex      { get; set; }
-        int    jmbg     { get; set; }
+        int    umcn     { get; set; } //Unique Master Citizen Number-JMBG
 
     }
 }
